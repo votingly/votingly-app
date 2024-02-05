b25c904d0b4ab1795ebe3b27d3ba8832fb67dfce
Nathan <nathan.vannieuwstadt@student.kdg.be>
2024-02-05 15:42:42 +0100

    Initial commit

---
+++ src/main/java/com/example/integration4/Integration4Application.java
@@ -0,0 +1,14 @@
+package com.example.integration4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class VotinglyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotinglyApplication.class, args);
+    }
+
+}
