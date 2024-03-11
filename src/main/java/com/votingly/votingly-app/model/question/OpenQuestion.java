ce94921b0b12e68a57eba13cfd9174fe4d146303
anna.deineko <anna.deineko@student.kdg.be>
2024-03-11 13:25:40 +0100

    js

--- src/main/java/com/votingly/votingly-app/model/question/OpenQuestion.java
+++ src/main/java/com/votingly/votingly-app/model/question/OpenQuestion.java
@@ -1,12 +1,16 @@
 package com.votingly.votingly-app.model.question;
 
 import com.votingly.votingly-app.model.QuestionType;
+import jakarta.persistence.Entity;
 
+@Entity
 public class OpenQuestion extends Question {
+
     public OpenQuestion() {
     }
 
     public OpenQuestion(long id, String questionName, QuestionType questionType) {
         super(id, questionName, questionType);
+//        this.questionType = QuestionType.OPEN;
     }
 }
