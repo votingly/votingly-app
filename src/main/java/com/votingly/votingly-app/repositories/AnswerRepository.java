75c26a4f381a169c0b6d2a1a1b7b25459fca2a6b
anna.deineko <anna.deineko@student.kdg.be>
2024-03-18 16:35:41 +0100

    save answers broken

--- src/main/java/com/votingly/votingly-app/repositories/AnswerRepository.java
+++ src/main/java/com/votingly/votingly-app/repositories/AnswerRepository.java
@@ -3,6 +3,7 @@ package com.votingly.votingly-app.repositories;
 import com.votingly.votingly-app.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
--- src/main/java/com/votingly/votingly-app/repositories/QuestionsRepository.java
+++ src/main/java/com/votingly/votingly-app/repositories/QuestionsRepository.java
@@ -1,5 +1,6 @@
 package com.votingly.votingly-app.repositories;
 
+import com.votingly.votingly-app.model.Answer;
 import com.votingly.votingly-app.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,5 @@ public interface QuestionsRepository extends JpaRepository<Question, Long> {
     List<Question> findAllQuestions();
 
     Question findById(long id);
+
 }
