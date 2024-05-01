2cb9e3379174f414572994eb02bb6eb070b35fc3
Sofiia Hmyria <sofiia.hmyria@student.kdg.be>
2024-05-01 16:28:41 +0200

    login and signup page design, issues changed

--- src/main/java/com/votingly/votingly-app/controller/mvc/SignUpController.java
+++ src/main/java/com/votingly/votingly-app/controller/mvc/SignUpController.java
@@ -1,12 +1,24 @@
 package com.votingly.votingly-app.controller.mvc;
 
+import com.votingly.votingly-app.controller.api.dto.user.UserDto;
 import com.votingly.votingly-app.model.User;
 import com.votingly.votingly-app.repositories.UserRepository;
+import com.votingly.votingly-app.security.CustomUserDetails;
+import com.votingly.votingly-app.service.UserService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class SignUpController {
@@ -18,29 +30,65 @@ public class SignUpController {
         return "signup";
     }
 
-//    @PostMapping("/signup")
-//    public String processSignupForm(@RequestParam("firstName") String firstName,
-//                                    @RequestParam("lastName") String lastName,
-//                                    @RequestParam("email") String email,
-//                                    @RequestParam("password") String password) {
-//        User user = new User();
-//        user.setFirstName(firstName);
-//        user.setLastName(lastName);
-//        user.setEmail(email);
-//        user.setPassword(password);
-//
-//        userRepository.save(user);
-//
-//        return "redirect:/";
-//    }
-
-    @PostMapping("/process_register")
-    public String processRegister(User user) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-
-        userRepository.save(user);
-        return "login";
-    }
 }
+
+//@Controller
+//public class SignUpController {
+//    private final UserRepository userRepository;
+//    private final UserService userService;
+//    private final ModelMapper modelMapper;
+//
+//    public SignUpController(UserRepository userRepository, UserService userService, ModelMapper modelMapper) {
+//        this.userRepository = userRepository;
+//        this.userService = userService;
+//        this.modelMapper = modelMapper;
+//    }
+//
+//
+//    @GetMapping("/signup")
+//    public String showSignupPage(Model model) {
+//        model.addAttribute("user", new User());
+//        return "signup";
+//    }
+//
+////    @PostMapping("/signup")
+////    public String processSignupForm(@RequestParam("firstName") String firstName,
+////                                    @RequestParam("lastName") String lastName,
+////                                    @RequestParam("email") String email,
+////                                    @RequestParam("password") String password) {
+////        User user = new User();
+////        user.setFirstName(firstName);
+////        user.setLastName(lastName);
+////        user.setEmail(email);
+////        user.setPassword(password);
+////
+////        userRepository.save(user);
+////
+////        return "redirect:/";
+////    }
+//
+//
+//    @PostMapping
+//    ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto userDto,
+//                                     @AuthenticationPrincipal CustomUserDetails user) {
+////        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+////        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+//        var createdUser = userService.addUser(
+//                userDto.getFirstName(), userDto.getLastName(), userDto.getEmail(), userDto.getPassword());
+//        return new ResponseEntity<>(
+//                modelMapper.map(createdUser, UserDto.class),
+//                HttpStatus.CREATED
+//        );
+//    }
+//
+////    @PostMapping("/process_register")
+////    public String processRegister(User user) {
+////        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+////        String encodedPassword = passwordEncoder.encode(user.getPassword());
+////        user.setPassword(encodedPassword);
+////
+////        userRepository.save(user);
+////        return "login";
+////    }
+//}
+
--- src/main/java/com/votingly/votingly-app/service/UserService.java
+++ src/main/java/com/votingly/votingly-app/service/UserService.java
@@ -2,7 +2,11 @@ package com.votingly.votingly-app.service;
 
 import com.votingly.votingly-app.model.User;
 import com.votingly.votingly-app.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserService {
@@ -19,4 +23,30 @@ public class UserService {
     public User getUserById(long id) {
         return userRepository.findById(id);
     }
+
+//    public User getUserByEmail(String email) {
+//        return userRepository.findByEmail(email);
+//    }
+//
+//    @Transactional
+//    public User addUser(String firstName, String lastName, String email, String password){
+////        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+////        String encodedPassword = passwordEncoder.encode(password);
+//        var user = new User(firstName, lastName, email, password);
+//        var savedUser = userRepository.save(user);
+//        return savedUser;
+//    }
+
+    public User addUser(String firstName, String lastName, String email, String password) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        // You might want to perform additional checks or operations here
+
+        return userRepository.save(user);
+    }
+
 }
