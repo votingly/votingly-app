79bbb320014156f5a2a1f0caf1af69416dfc4226
Rossy <git.umbrella830@slmails.com>
2024-05-04 20:37:51 +0000

    CICD finishing deployment

--- deploy/scripts/connect_to_vm.sh
+++ deploy/scripts/connect_to_vm.sh
@@ -3,4 +3,4 @@
 # Pre-requisites:
 # gcloud installed and authenticated
 # your ip range is allowed in firewall ssh (you can temporarily add 0.0.0.0/0 for all internet if you don't know your ip range)
-gcloud compute ssh --zone "europe-west1-b" "votingly-testing-vm" --project "votingly"
+gcloud compute ssh "votingly-testing-vm" --zone "europe-west1-b" --project "votingly"
