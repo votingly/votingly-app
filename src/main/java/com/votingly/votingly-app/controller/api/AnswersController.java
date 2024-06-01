9b737616da73d936a0b5068fedb058dc392db0c9
anna.deineko <anna.deineko@student.kdg.be>
2024-06-02 00:46:39 +0200

    export of open and range answers values

--- src/main/java/com/votingly/votingly-app/controller/api/AnswersController.java
+++ src/main/java/com/votingly/votingly-app/controller/api/AnswersController.java
@@ -4,6 +4,8 @@ import com.votingly.votingly-app.controller.api.dto.answer.AnswerDto;
 import com.votingly.votingly-app.controller.api.dto.answer.NewAnswerDto;
 import com.votingly.votingly-app.converters.ChoiceAnswerDtoConverter;
 import com.votingly.votingly-app.model.answers.Answer;
+import com.votingly.votingly-app.model.answers.OpenAnswer;
+import com.votingly.votingly-app.model.answers.RangeAnswer;
 import com.votingly.votingly-app.model.question.Question;
 import com.votingly.votingly-app.security.CustomUserDetails;
 import com.votingly.votingly-app.service.AnswerService;
@@ -98,18 +100,31 @@ public class AnswersController {
         List<Answer> answers = answerService.getAllSurveys(surveyId);
 
         if (answers.isEmpty()) {
-            return ResponseEntity.noContent().build(); // No content to export
+            return ResponseEntity.noContent().build();
         }
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        try (CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), CSVFormat.DEFAULT.withHeader("AnswerID", "SurveyID", "UserID", "QuestionID", "AnswerTime"))) {
+        try (CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), CSVFormat.DEFAULT.withHeader("AnswerID", "SurveyID", "UserID", "QuestionType", "AnswerValue", "AnswerTime"))) {
             for (Answer answer : answers) {
-                csvPrinter.printRecord(answer.getAnswerId(), surveyId, answer.getUserId(), answer.getQuestion(), answer.getAnswerTime());
+                String answerType = answer.getClass().getSimpleName();
+
+                switch (answerType) {
+                    case "OpenAnswer":
+                        OpenAnswer openAnswer = (OpenAnswer) answer;
+                        csvPrinter.printRecord(openAnswer.getAnswerId(), openAnswer.getSurveyId(), openAnswer.getUserId(), answerType, openAnswer.getAnswer(), openAnswer.getAnswerTime());
+                        break;
+                    case "RangeAnswer":
+                        RangeAnswer rangeAnswer = (RangeAnswer) answer;
+                        csvPrinter.printRecord(rangeAnswer.getAnswerId(), rangeAnswer.getSurveyId(), rangeAnswer.getUserId(), answerType, rangeAnswer.getRange_answer(), rangeAnswer.getAnswerTime());
+                        break;
+                    // TODO: Add more cases for other types of answers
+                    default:
+                        break;
+                }
             }
             csvPrinter.flush();
         } catch (IOException e) {
-            // Log the exception and return an internal server error response
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
