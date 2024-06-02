9a338358de34e0096a50a363cb7d4ae4ac8b8c00
anna.deineko <anna.deineko@student.kdg.be>
2024-06-02 19:58:05 +0200

    survey id in details API

--- src/main/java/com/votingly/votingly-app/controller/api/dto/questions/QuestionDto.java
+++ src/main/java/com/votingly/votingly-app/controller/api/dto/questions/QuestionDto.java
@@ -1,7 +1,5 @@
 package com.votingly.votingly-app.controller.api.dto.questions;
 
-import com.votingly.votingly-app.model.Survey;
-import com.votingly.votingly-app.model.question.Question;
 import com.votingly.votingly-app.model.question.QuestionType;
 
 
@@ -29,14 +27,6 @@ public class QuestionDto {
         this.questionType = questionType;
         this.surveyId = surveyId;
     }
-
-//    public QuestionDto(Question question) {
-//        this.id = question.getId();
-//        this.questionName = question.getQuestionName();
-//        this.questionType = question.getQuestionType();
-//        this.surveyId = question.getSurvey();
-//    }
-
     public long getId() {
         return id;
     }
