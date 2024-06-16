d2d9a7460ea54b4b53bcbc152047d98e335a156e
anna.deineko <anna.deineko@student.kdg.be>
2024-06-16 16:23:21 +0200

    Answers per day statistics

---
+++ src/main/java/com/votingly/votingly-app/controller/api/dto/answer/UserAnswerStatisticsDto.java
@@ -0,0 +1,29 @@
+package com.votingly.votingly-app.controller.api.dto.answer;
+
+import java.time.LocalDate;
+
+public class UserAnswerStatisticsDto {
+    private LocalDate answerTime;
+    private long answerCount;
+
+    public UserAnswerStatisticsDto(LocalDate answerTime, long answerCount) {
+        this.answerTime = answerTime;
+        this.answerCount = answerCount;
+    }
+
+    public LocalDate getAnswerTime() {
+        return answerTime;
+    }
+
+    public void setAnswerTime(LocalDate answerTime) {
+        this.answerTime = answerTime;
+    }
+
+    public long getAnswerCount() {
+        return answerCount;
+    }
+
+    public void setAnswerCount(long answerCount) {
+        this.answerCount = answerCount;
+    }
+}
